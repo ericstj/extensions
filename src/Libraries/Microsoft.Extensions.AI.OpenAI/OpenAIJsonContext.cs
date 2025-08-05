88ef6b3e0d86812bdc97ede228f1633fd4d6aaa8
Eric StJohn <ericstj@microsoft.com>
2025-08-04 21:06:36 -0700

    Prototype of using ImageGenerationTool

--- src/Libraries/Microsoft.Extensions.AI.OpenAI/OpenAIJsonContext.cs
+++ src/Libraries/Microsoft.Extensions.AI.OpenAI/OpenAIJsonContext.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.AI;
     WriteIndented = true)]
 [JsonSerializable(typeof(OpenAIClientExtensions.ToolJson))]
 [JsonSerializable(typeof(IDictionary<string, object?>))]
+[JsonSerializable(typeof(IDictionary<string, string?>))]
 [JsonSerializable(typeof(string[]))]
 [JsonSerializable(typeof(JsonElement))]
 internal sealed partial class OpenAIJsonContext : JsonSerializerContext;
